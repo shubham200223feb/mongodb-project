02498e2c75c4aa757a4df848aa406a739d71ea21
shubham200223feb <shubham2101861@gmail.com>
2025-09-14 20:37:55 +0530

    Add files via upload

---
+++ till cart/util/database.js
@@ -0,0 +1,29 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+
+const mongoConnect = callback => {
+  MongoClient.connect(
+    "mongodb+srv://shubhamtesting:o30QALQHLFG9SMM9@cluster0.fgu9rgz.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0"
+  )
+    .then(client => {
+      console.log('Connected!');
+      _db = client.db();
+      callback();
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
